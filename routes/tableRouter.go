af95e87e7d079d772c7d406fef711239970604ab
Pranav <pranavmasekar4@gmail.com>
2022-05-09 21:30:09 +0530

    Project Complete

--- routes/tableRouter.go
+++ routes/tableRouter.go
@@ -9,5 +9,5 @@ func TableRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/tables", controllers.GetTables())
 	incomingRoutes.GET("/tables/:table_id", controllers.GetTable())
 	incomingRoutes.POST("/tables", controllers.CreateTable())
-	incomingRoutes.POST("/tables/table_id", controllers.UpdateTable())
+	incomingRoutes.POST("/tables/:table_id", controllers.UpdateTable())
 }
